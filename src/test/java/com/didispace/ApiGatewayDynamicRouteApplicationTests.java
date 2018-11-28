756ae8b62b252404da854612d9db30af4a4d7f15
xjx <xjx@qq.com>
2018-11-28 17:08:52 +0800

    xjx

---
+++ src/test/java/com/didispace/ApiGatewayDynamicRouteApplicationTests.java
@@ -0,0 +1,18 @@
+package com.didispace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootApplication
+@SpringApplicationConfiguration(ApiGatewayDynamicRouteApplication.class)
+public class ApiGatewayDynamicRouteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
